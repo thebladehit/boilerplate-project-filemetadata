7286552d07b21bba33e40e501c59d0457aeffae8
thebladehit <boqdanyarmolka@gmail.com>
2025-04-09 14:48:33 +0300

    Implement File Metadata Microservice

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-var app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +15,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('Finle not found');
+  }
+  const { originalname, mimetype, size } = req.file;
 
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size,
+  });
+});
 
 
 const port = process.env.PORT || 3000;
